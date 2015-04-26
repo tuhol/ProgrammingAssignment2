e63146c9dcb51418e86539d847c07c63edf17a61
tuhol <tuhol1081@o2.pl>
2015-04-26 10:49:05 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The idea of those two functions is to return the inverse matrix if that exists and
+## not to compute the matrix if nothing changed
 
-## Write a short comment describing this function
+
+## Function will return the list of matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inverseofmatrix <- NULL #seting value of inverse matrix to Null
+  set <- function(y){ #Function set for caching the value
+    X<<-y
+    inverseofmatrix <<-NULL # if the matrix was change inverseofmatrix set to Null 
+  }
+  get <- function() x #returns value of inverse
+  setinverse <- function(solve) inverseofmatrix <<- solve #calculates the inverse of the matrix
+  getinverse <- function() inverseofmatrix # gets the inverse
+  list(set = set, get = get,     # product of the whle function
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function will return the cache of matrix if needed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverseofmatrix <- x$getinverse()
+  if(!is.null(inverseofmatrix)) {                 
+    message("getting cached data - Inverse of the matrix") #if the inverse matrix exists then we return it
+    return(inverseofmatrix)
+  }
+  d<- det(x$get()) # check if we can inverse the matrix (det!=0)
+  if(d != 0){  
+    data <- x$get()                               
+    inverseofmatrix <- solve(data, ...) #calculating the inverse
+    x$setinverse(inverseofmatrix)
+    inverseofmatrix
+  } else {print("No inverse possible")}
 }
